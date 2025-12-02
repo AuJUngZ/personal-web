78d741e6bbd8024c8d53c68da23d1eafa85c7033
AuJUngZ <nuttapong6280@gmail.com>
2025-12-02 15:33:25 +0700

    fix: update Analytics import to use the correct package

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
